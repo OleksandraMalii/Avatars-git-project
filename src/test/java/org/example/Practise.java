1aba2928b9a859c9572532e465512037ed1d8443
OleksandraMalii <sashaproskurina09@gmail.com>
2024-06-26 20:34:30 -0500

    My fourth push

---
+++ src/test/java/org/example/Practise.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Practise {
+    public static void  main(String[] args){
+        System.out.println("Hello class");
+        System.out.println("I want to get a new job");
+        System.out.println("But first I need a vacation");
+        System.out.println("Dreams come true");
+        System.out.println("All will be OK");
+    }
+}
